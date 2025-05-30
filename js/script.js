a694f8984d86540bb4edbfbebad91848ce2b75f2
Tallysson2k23 <tallysson.bldd@gmail.com>
2025-05-30 09:51:14 -0300

    atualização

--- js/script.js
+++ js/script.js
@@ -1,26 +1,14 @@
 const equipes = [
-<<<<<<< HEAD
-  { nome: 'ROBSON e NETO', os: 82 },
-  { nome: 'JEAN e MATHEUS', os: 20 },
-  { nome: 'RANIERE e MATEUS H', os: 113 },
-  { nome: 'GILVANIO e JORGE', os: 52 },
-  { nome: 'MARCIO e JEFFERSON', os: 106 },
-  { nome: 'LUCAS ER. e GABRIEL', os: 164 },
-  { nome: 'MARIO e IGO', os: 106 },
-  { nome: 'BONAPARTE e WILLAMYS', os: 106 },
-  { nome: 'BERLY', os: 92 }
-=======
-  { nome: 'NETO e WILLAMYS', os: 118 },
-  { nome: 'ROBSON e MATHEUS S.', os: 68 },
-  { nome: 'JEAN e RANIERE', os: 54 },
-  { nome: 'GILVANIO e IGO', os: 121 },
-  { nome: 'MARCIO e JORGE', os: 94 },
-  { nome: 'LUCAS ER. e GABRIEL', os: 139 },
-  { nome: 'MARIO e BONAPARTE', os: 115 },
-  { nome: 'MATEUS H.', os: 33 },
-  { nome: 'JEFFERSON', os: 74 },
-  { nome: 'BERLY', os: 56 }
->>>>>>> a8760a45ebc162d4775c99fc63ba81b691e75179
+  { nome: 'ROBSON e NETO', os: 12 },
+  { nome: 'JEAN e MATHEUS', os: 8 },
+  { nome: 'RANIERE e MATEUS H', os: 15 },
+  { nome: 'GILVANIO e JORGE', os: 9 },
+  { nome: 'MARCIO e JEFFERSON', os: 11 },
+  { nome: 'LUCAS ER. e GABRIEL', os: 10 },
+  { nome: 'MARIO e IGO', os: 14 },
+  { nome: 'BONAPARTE', os: 6 },
+  { nome: 'WILLAMYS', os: 13 },
+  { nome: 'BERLY', os: 7 }
 ];
 
 // Ordena do maior para o menor
